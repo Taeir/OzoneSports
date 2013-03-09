dae61dcbeb05d2683fde8f481a84fd5fe133da27
Taico Aerts <taicoaerts@hotmail.com>
2013-03-09 23:22:17 +0100

    - Changed ozonesports to mcsports in permhandler - Removed silly Perm enum that we will never use...

--- src/com/aurean/mcsports/PermHandler.java
+++ src/com/aurean/mcsports/PermHandler.java
@@ -4,19 +4,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class PermHandler {
-	public static enum Perm{
-		reload
-	};
-	
 	public static boolean has(CommandSender sender, String shortPermission){
-		return sender.hasPermission("ozonesports."+shortPermission);
+		return sender.hasPermission("mcsports."+shortPermission);
 	}
 	public static boolean has(Player player, String shortPermission){
-		return player.hasPermission("ozonesports."+shortPermission);
-	}
-	public static boolean has(Player player, Perm permission){
-		if (permission == Perm.reload)
-			return player.hasPermission("ozonesports.mod.reload");
-		return false;
+		return player.hasPermission("mcsports."+shortPermission);
 	}
 }
