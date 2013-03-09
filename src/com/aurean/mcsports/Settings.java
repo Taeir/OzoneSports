06e18ddaecf9f190295dcf961b314dff3bf362f7
Taico Aerts <taicoaerts@hotmail.com>
2013-03-09 14:30:20 +0100

    Added settings

---
+++ src/com/aurean/mcsports/Settings.java
@@ -0,0 +1,8 @@
+package com.aurean.mcsports;
+
+import org.bukkit.Material;
+
+public class Settings {
+	public static Material EntityBall = Material.SLIME_BALL;
+	public static String GoalMessage = "Goal! For &team!";
+}
