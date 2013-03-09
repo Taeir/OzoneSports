d14a983b50fc0ba85c44c7889703f419086fd8eb
Taico Aerts <taicoaerts@hotmail.com>
2013-03-09 14:43:58 +0100

    Added comment to Settings and testing commiting

--- src/com/aurean/mcsports/Settings.java
+++ src/com/aurean/mcsports/Settings.java
@@ -5,4 +5,5 @@ import org.bukkit.Material;
 public class Settings {
 	public static Material EntityBall = Material.SLIME_BALL;
 	public static String GoalMessage = "Goal! For &team!";
+	//TODO make sure the settings get updated from the config files.
 }
