7ade49bc98576f264af84e3b365cdfa85a26c7b1
Taico Aerts <taicoaerts@hotmail.com>
2013-03-09 13:14:34 +0100

    Added goal object

---
+++ src/com/aurean/mcsports/objects/Goal.java
@@ -0,0 +1,35 @@
+package com.aurean.mcsports.objects;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+import com.aurean.mcsports.GameField;
+
+public class Goal {
+	GameField field;
+	public String name;
+	Location corner1;
+	Location corner2;
+	
+	public Goal(String name, Location l1, Location l2, GameField field){
+		this.name = name;
+		this.corner1 = l1;
+		this.corner2 = l2;
+		this.field = field;
+	}
+	
+	public Location[] getCorners(){
+		return new Location[] { corner1, corner2 };
+	}
+	
+	public GameField getField(){
+		return field;
+	}
+	public boolean containsBlock(Location loc){
+		Vector v1 = Vector.getMinimum(corner1.toVector(), corner2.toVector());
+		Vector v2 = Vector.getMaximum(corner1.toVector(), corner2.toVector());
+		return (loc.getX() >= v1.getX()) && (loc.getX() <= v2.getX()) &&
+				(loc.getZ() >= v1.getZ()) && (loc.getZ() <= v2.getZ());
+		//returns true if the coords are within the 2 corners.
+	}
+}
